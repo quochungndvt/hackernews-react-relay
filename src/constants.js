24e4e4eef48d86d768d3ad0ceaaeaf5b7d495379
francolin <quochungndvt@gmail.com>
2019-03-11 11:03:35 +0700

    fix limit items

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const GC_USER_ID = 'graphcool-user-id'
 export const GC_AUTH_TOKEN = 'graphcool-auth-token'
-export const ITEMS_PER_PAGE = 5 // setting this only to one so you can easily test your pagination implementation
+export const ITEMS_PER_PAGE = 20 // setting this only to one so you can easily test your pagination implementation
